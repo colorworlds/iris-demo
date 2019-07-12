51200261aba71d51d55524b5496898a6c6778a39
wyanlord <381433852@qq.com>
2019-07-12 10:30:54 +0800

    Update main.go

--- main.go
+++ main.go
@@ -4,20 +4,26 @@ import (
 	"IRIS_WEB/conf"
 	"IRIS_WEB/http"
 	"IRIS_WEB/utility/db"
+	"IRIS_WEB/utility/log"
 	"flag"
 	"fmt"
 	"os"
 )
 
-func init() {
+func main() {
 	flag.Parse()
 	checkErr("InitConfig", conf.InitConfig())
+
+	defer log.CloseLogger()
+	checkErr("InitLogger", log.InitLogger(&conf.Conf.Logger))
 
 	defer db.CloseMysql()
 	checkErr("InitMysql", db.InitMysql(&conf.Conf.Mysql))
 
 	defer db.CloseRedis()
 	checkErr("InitRedis", db.InitRedis(&conf.Conf.Redis))
+
+	http.RunIris(conf.Conf.Server.Port)
 }
 
 func checkErr(errMsg string, err error) {
@@ -25,8 +31,4 @@ func checkErr(errMsg string, err error) {
 		fmt.Printf(errMsg + " Error: %v\n", err)
 		os.Exit(1)
 	}
-}
-
-func main() {
-	http.RunIris(conf.Conf.Server.Port)
 }
